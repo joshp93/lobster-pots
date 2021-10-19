f034abb8ff2e212750e46224439a754c91679592
Kemosabe <joshuapearson1993gmail.com>
2021-10-19 22:01:27 +0100

    Updates to test and added ng deploy

--- src/app/classes/round-setup.spec.ts
+++ src/app/classes/round-setup.spec.ts
@@ -3,6 +3,6 @@ import { RoundSetup } from './round-setup';
 
 describe('RoundSetup', () => {
   it('should create an instance', () => {
-    expect(new RoundSetup('title', 'info', WeatherConditions.perfect, false, 10, 10, 10, "Hello")).toBeTruthy();
+    expect(new RoundSetup()).toBeTruthy();
   });
 });
